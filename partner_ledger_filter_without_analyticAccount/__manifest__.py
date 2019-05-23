0806c977d02578ccc6a874255f6b7a0b8595b8ac
GlobalItech <30366468+GlobalItech@users.noreply.github.com>
2019-05-23 11:46:13 +0500

    Update __manifest__.py

--- partner_ledger_filter_without_analyticAccount/__manifest__.py
+++ partner_ledger_filter_without_analyticAccount/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Account Filter and Partner Filters on Ledger Reports(Community',
+    'name': 'Account Filter and Partner Filters on Ledger Reports(Community)',
     'version': '1.0',
     'category': 'Accounting',
     'author':'itech resources',
